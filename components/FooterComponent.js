d048fb20e742a11d3209e20e87e3ba9593a86a0e
aripan <aripan017@gmail.com>
2021-08-07 19:30:43 +0200

    UI is done

--- components/FooterComponent.js
+++ components/FooterComponent.js
@@ -3,17 +3,15 @@ import styles from "../styles/Layout.module.css"
 const FooterComponent = () => {
     return (
        <div> 
-      
-       
-<div className="w-7/12 h-20 border-solid border-2 border-black mx-auto mt-6">
-    
-    <img className={` ${styles["profile-img"]} transform -translate-y-14 z-50 mx-auto border-solid border-2 border-black`}
-       src="/images/avatar.png" 
-       height={80} 
-       width={80} 
-       alt="Your Name"
-   />
-   </div></div>
+            <div className="w-7/12 h-20 mx-auto mt-6 bg-white">
+            <img className={` ${styles["profile-img"]} transform -translate-y-14 z-50 mx-auto    border-solid border-2 border-black`}
+                    src="/images/avatar.png" 
+                    height={80} 
+                    width={80} 
+                    alt="Your Name"
+            />
+            </div>
+          </div>
        
         
     )
--- components/HeaderComponent.js
+++ components/HeaderComponent.js
@@ -1,8 +1,10 @@
 
 const HederComponent = () => {
     return (
-        <div className="w-7/12 h-20 border-solid border-2 border-black mx-auto text-center mb-6">
-            <h1>HederComponent will be here</h1>
+        <div className="w-7/12 h-20 mx-auto text-center mb-6 flex justify-start items-center bg-white">
+           <img className="w-20 object-contain" src="https://media-exp1.licdn.com/dms/image/C560BAQFmCkQU08DcJg/company-logo_200_200/0/1625678982747?e=1636588800&v=beta&t=1nSU8K9kSEt6iN_w9KX6wVbwrKTN7COxiiAD0CEUiq0" /> 
+           <span className="font-bold ml-2">Meta.</span>link
+           
         </div>
     )
 }
